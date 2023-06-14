42eab827f67c86adcf74da3f56f0dd818c4849fe
Andrey Savrasov <savrasov.dev@gmail.com>
2023-06-14 10:20:15 +0300

    Add test BubbleSortTest

---
+++ tests/Sort/BubbleSortTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Sort;
+
+use App\Sort\BubbleSort;
+use PHPUnit\Framework\TestCase;
+
+class BubbleSortTest extends TestCase
+{
+
+    public function testIterative()
+    {
+        $firstList = range(0, 1000, 3);
+        shuffle($firstList);
+
+        $secondList = $firstList;
+
+        sort($firstList);
+        (new BubbleSort())->iterative($secondList);
+
+        $this->assertEquals($firstList, $secondList);
+    }
+}
