a05ac35ad425ad87be26582638684ce280e14676
Andrey Savrasov <savrasov.dev@gmail.com>
2023-06-14 10:33:58 +0300

    Patch test BubbleSortTest

--- tests/Sort/BubbleSortTest.php
+++ tests/Sort/BubbleSortTest.php
@@ -10,7 +10,7 @@ class BubbleSortTest extends TestCase
 
     public function testIterative()
     {
-        $firstList = range(0, 1000, 3);
+        $firstList = range(0, 3000, 3);
         shuffle($firstList);
 
         $secondList = $firstList;
